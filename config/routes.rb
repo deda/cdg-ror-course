9605dfb356c6a6f5289fcef4c56cfe5cc5b6bce0
Alex Melnikov <grandpa.ape@gmail.com>
2020-04-28 22:41:58 +0300

    pretty urls for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   get 'home/index'
  
-  resources :users
+#  resources :users
   resources :posts
 
 
