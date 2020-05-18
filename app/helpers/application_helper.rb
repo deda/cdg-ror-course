a0a6ac9c9d8f6ee23577018e8a2eadef711c3997
Alex Melnikov <grandpa.ape@gmail.com>
2020-05-19 00:27:57 +0300

    now i know button_tag helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,10 +11,11 @@ module ApplicationHelper
       def flash_messages(opts = {})
         flash.each do |msg_type, message|
           concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: "alert") do 
-                  concat content_tag(:button, '&times;'.html_safe, class: "close", data: { dismiss: 'alert' })
+                  concat button_tag '&times;'.html_safe, class: "close", data: { dismiss: 'alert' }
                   concat message 
                 end)
         end
         nil
       end  
 end
+#button_tag "&times;", class: 'close', data: { dismiss: 'alert' })
